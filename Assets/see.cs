cf4e88b936b2bf0892976e382f653dee7786ac6b
Tsun Leung <beston.leung@mail.utoronto.ca>
2017-01-22 06:44:22 -0500

    First try
    This is the initial try to use the camera and render the foottage on the
    canvas

---
+++ Assets/see.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class see : MonoBehaviour
+{
+    void Start()
+    {
+        WebCamTexture webcamTexture = new WebCamTexture(1280, 720, 25);
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+
+    private void Update()
+    {
+        WebCamTexture wct = new WebCamTexture(1280, 720, 25);
+        if (wct.width < 100)
+        {
+            Debug.Log("Still waiting another frame for correct info...");
+            return;
+        }
+
+        // change as user rotates iPhone or Android:
+
+        int cwNeeded = wct.videoRotationAngle;
+        // Unity helpfully returns the _clockwise_ twist needed
+        // guess nobody at Unity noticed their product works in counterclockwise:
+        int ccwNeeded = -cwNeeded;
+
+        // IF the image needs to be mirrored, it seems that it
+        // ALSO needs to be spun. Strange: but true.
+        if (wct.videoVerticallyMirrored) ccwNeeded += 180;
+    }
+}
